a89c11939e74ada4bc931fa4fb7e8b43c14bce1a
Mikhail Lebedinets <mhlebd@gmail.com>
2023-09-11 16:30:32 +0300

    Added permissions support

--- src/main/java/ru/lebedinets/mc/oldnetheritesmithing/OldNetheriteSmithing.java
+++ src/main/java/ru/lebedinets/mc/oldnetheritesmithing/OldNetheriteSmithing.java
@@ -15,10 +15,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
+/*
+ * There used to be a lot of code here...
+ * Initially, I simply listen for event when a player put items into smithing table and give result item to him.
+ * Then I automatically inserted upgrade templates into slot and didn't let the player take them.
+ * In the end, it turned out to be so simple that I'm even ashamed.
+ * I found already existing plugin https://github.com/TrollsterCooleg/NoNetheriteTemplate,
+ * and there was a proper implementation that simply adds additional recipes to the smithing table.
+ * That plugin implementation doesn't have the option to set permissions because it simply globally registers new recipes.
+ * So, use this plugin only if you need permissions; otherwise, use the one that was provided earlier.
+ */
+
 public final class OldNetheriteSmithing extends JavaPlugin implements Listener {
 
     private static final Map<Material, Material> resultMap = new HashMap<>();
-    private final ArrayList<UUID> currentlySmithingPlayers = new ArrayList<>();
+    private final Set<UUID> currentlySmithingPlayers = new HashSet<>();
     private final UpdateChecker updateChecker = new UpdateChecker(this.getDescription(), this.getServer());
 
     private void populateResultMap() {
@@ -57,7 +68,11 @@ public final class OldNetheriteSmithing extends JavaPlugin implements Listener {
     public void onInventoryClose(InventoryCloseEvent event) {
         Inventory closedInventory = event.getInventory();
         Player player = (Player) event.getPlayer();
-        if (closedInventory.getType() == InventoryType.SMITHING && closedInventory.getItem(0) != null && Objects.requireNonNull(closedInventory.getItem(0)).getType() == Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE) {
+        if (closedInventory.getType() == InventoryType.SMITHING &&
+                closedInventory.getItem(0) != null &&
+                Objects.requireNonNull(closedInventory.getItem(0)).getType() == Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE &&
+                currentlySmithingPlayers.contains(player.getUniqueId())
+        ) {
             ItemStack item1 = closedInventory.getItem(1);
             ItemStack item2 = closedInventory.getItem(2);
             if (item1 != null) {
@@ -68,6 +83,7 @@ public final class OldNetheriteSmithing extends JavaPlugin implements Listener {
             }
             closedInventory.clear();
         }
+        currentlySmithingPlayers.remove(player.getUniqueId());
     }
 
     @EventHandler
@@ -86,6 +102,10 @@ public final class OldNetheriteSmithing extends JavaPlugin implements Listener {
     public void onPrepareSmithing(PrepareSmithingEvent event) {
         ItemStack[] inputItems = event.getInventory().getContents();
         Player player = (Player) event.getView().getPlayer();
+
+        if (player.isPermissionSet("oldnetheritesmithing.smith") && !player.hasPermission("oldnetheritesmithing.smith")) {
+            return;
+        }
 
         if ((inputItems[1] == null || inputItems[2] == null) && currentlySmithingPlayers.contains(player.getUniqueId())) {
             event.getInventory().removeItem(new ItemStack(Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE));
